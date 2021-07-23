4d24a69ec51ef69daf2af30eb2b2c16ef86a33ab
Adi Skop <adiskop@gmail.com>
2021-07-23 15:19:54 +0200

    add create method in travelgems controller

--- app/controllers/api/v1/travelgems_controller.rb
+++ app/controllers/api/v1/travelgems_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::TravelgemsController < ApplicationController
+
+    def index 
+        travelgems = Travelgem.all
+        render json: travelgems
+    end
+
+    def create
+        travelgem = Travelgem.new(travelgem_params)
+        if travelgem.save
+            render json: travelgem, status: :accepted 
+        else
+            render json: {error:travelgem.error.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+    def travelgem_params
+        #require this hash , and permit only this attribute to be taken into 
+        #our db and creating a new instance
+        params.require(:travelgem).permit(:title, :description, :image_url, :destination_id)
+    end
+
 end
