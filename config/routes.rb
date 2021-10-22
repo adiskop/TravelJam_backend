d8670ebe849fab0d1a430b375bc027b11ee2f86c
Adi Skop <adiskop@gmail.com>
2021-10-22 12:28:30 +0200

    heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.default_url_options[:host] = "http://traveljam.herokuapp.com/"
+Rails.application.routes.default_url_options[:host] = "http://murmuring-ravine-16588.herokuapp.com/"
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
